068201d767fb6190b0393eb5ba5e239162437a31
Brian Salvador <brian.herom@proton.me>
2023-04-18 21:06:37 -0400

    add all content to continue the course

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -114,31 +114,19 @@ def enroll(request, course_id):
 def submit(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     user = request.user
+    enrrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrrollment)
 
-    submitted_answers = []
     for key in request.POST:
         if key.startswith('choice'):
             choice_id = int(request.POST[key])
-            submitted_answers.append(choice_id)
+            submission.choices.add(choice_id)
+    submission.save()
 
-    choices = Choices.objects.get(id__in=submitted_answers)
-    enrrollment = Enrollment.objects.get(user=user, course=course)
-    submission = submission.objects.create(enrollment=enrrollment, choices=choices)
-    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(submission.id,)))
+    return submission
+    return redirect(f'onlinecourse/{course_id}/submission/{submission.id}/result')
 
     
-
-# <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
-
-
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
 # you may implement it based on the following logic:
         # Get course and submission based on their ids
@@ -147,6 +135,7 @@ def submit(request, course_id):
         # Calculate the total score
 
 def show_exam_result(request, course_id, submission_id):
-
+    course = get_object_or_404(Course, pk=course_id)
+    return course
 
 
